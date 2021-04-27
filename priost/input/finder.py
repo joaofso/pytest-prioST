ca0904842a1d79044a3f873ad49d8ea8c14e4da9
João Felipe Silva Ouriques <joaofso@gmail.com>
2021-04-27 20:31:38 +0200

    Adding the source code finder for both pytest regular test cases and bdd test cases

--- priost/input/finder.py
+++ priost/input/finder.py
@@ -1,36 +1,49 @@
 from pytest import Function
 import os
 from typing import List
+import logging
+import inspect
+import importlib.util
+
 
 class SourceCodeFinder:
 
     BDD_ENDING = "pytest_bdd/scenario.py"
+
+    def __init__(self):
+        self.logger = self.set_logging()
+
+    @staticmethod
+    def set_logging():
+        logger = logging.getLogger("priost.SourceCodeFinder")
+        return logger
 
     def find_source_code(self, test_case: Function):
         location_info = test_case.location
         module_path = location_info[0]
         test_method_name = location_info[2]
         root_dir = test_case.fspath.dirname
-
-
         if module_path.endswith(self.BDD_ENDING):
             # look for feature file
+            self.logger.info(f"{test_case.name} is a BDD test case")
             return self._find_bdd_scenario(root_dir, test_method_name)
         else:
             # look for python code
-            self._find_python_test()
+            self.logger.info(f"{test_case.name} is a python test case")
+            self._find_python_test(test_case.fspath.purebasename, test_case.fspath.strpath, test_method_name)
 
     def _find_bdd_scenario(self, root: str, test_method_name: str) -> List[str]:
         scenario_name_tokens = test_method_name.split("_")[1:]
+        scenario_lines = []
         for dirpath, subdirs, filenames in os.walk(root):
             for file in filenames:
                 if file.endswith(".feature"):
                     full_path = os.path.join(dirpath, file)
                     with open(full_path, "r") as feature_file:
                         found_scenario = False
-                        scenario_lines = []
                         for line in feature_file:
                             if all(token in line for token in ["Scenario"] + scenario_name_tokens) and not found_scenario:
+                                self.logger.info(f"BDD scenario {test_method_name} found in {full_path}")
                                 found_scenario = True
                                 scenario_lines.append(line.strip())
                                 continue
@@ -41,6 +54,14 @@ class SourceCodeFinder:
                                     break
                         if found_scenario:
                             return scenario_lines
+        if not scenario_lines:
+            self.logger.error(f"The source for {test_method_name} was not found")
+            raise FileNotFoundError("")
 
-    def _find_python_test(self):
-        pass
+    def _find_python_test(self, module_name: str, module_path: str, test_method_name: str):
+        spec = importlib.util.spec_from_file_location(module_name, module_path)
+        module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
+        func = getattr(module, test_method_name)
+        self.logger.info(f"The test method {test_method_name} from {module_name} was found in {module_path}")
+        return inspect.getsource(func)
--- test/conftest.py
+++ test/conftest.py
@@ -2,13 +2,25 @@ from typing import List
 from priost.input.finder import SourceCodeFinder
 from _pytest.config import Config
 from pytest import *
+import logging
+
+def set_logging():
+    logger = logging.getLogger("priost")
+    logger.setLevel(logging.DEBUG)
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+    return logger
 
 
 def pytest_collection_modifyitems(session: Session, config: Config, items: List[Item]):
+    logger = set_logging()
     for tc in items:
+        logger.debug(f"Processing test case {tc.name}")
         finder = SourceCodeFinder()
         code = finder.find_source_code(tc)
-        print(code)
 
 
 
